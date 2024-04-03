de0c7e1b481202b781e8a70d51e632decf6f78b6
Adam MacDonald <am1macdonald@gmail.com>
2024-04-03 07:58:17 -0400

    Implement methods for chirp creation

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,23 +1,26 @@
 package database
 
 import (
+	"encoding/json"
 	"os"
 	"sync"
-
-	"github.com/am1macdonald/chirpy/internal/chirps"
 )
 
 const (
 	dbPath = "../../database.json"
 )
 
+type Chirp struct {
+	Body string `json:"body"`
+}
+
 type DB struct {
 	path string
-	mu   *sync.Mutex
+	mu   sync.Mutex
 }
 
 type DBStructure struct {
-	Chirps map[int]chirps.Chirp
+	Chirps map[int]Chirp `json:"chirps"`
 }
 
 func (db *DB) ensureDB() error {
@@ -27,12 +30,65 @@ func (db *DB) ensureDB() error {
 		if err != nil {
 			return err
 		}
+		err = db.writeDB(DBStructure{
+			Chirps: map[int]Chirp{},
+		})
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
+func (db *DB) loadDB() (*DBStructure, error) {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	bytes, err := os.ReadFile(db.path)
+	if err != nil {
+		return nil, err
+	}
+	dbs := DBStructure{}
+	err = json.Unmarshal(bytes, &dbs)
+	if err != nil {
+		return nil, err
+	}
+	return &dbs, nil
+}
+
+func (db *DB) writeDB(dbStructure DBStructure) error {
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	bytes, err := json.Marshal(dbStructure)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(db.path, bytes, os.FileMode(int(0777)))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *DB) CreateChirp(body string) (*Chirp, error) {
+	dbs, err := db.loadDB()
+	if err != nil {
+		return nil, err
+	}
+	chirp := Chirp{
+		Body: body,
+	}
+	dbs.Chirps[len(dbs.Chirps)+1] = chirp
+	err = db.writeDB(*dbs)
+	if err != nil {
+		return nil, err
+	}
+	return &chirp, nil
+}
+
 func NewDB() (*DB, error) {
-	db := DB{}
+	db := DB{
+		path: dbPath,
+	}
 	err := db.ensureDB()
 	if err != nil {
 		return nil, err
--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -7,19 +7,34 @@ import (
 	"github.com/am1macdonald/chirpy/internal/database"
 )
 
+func beforeEach() (*database.DB, error) {
+	os.Remove("../../database.json")
+	return database.NewDB()
+}
+
 // creates a new database_test
 func TestCreateDatabase(t *testing.T) {
-	os.Remove("../../database.json")
-	db, err := database.NewDB()
+	db, err := beforeEach()
 	if db == nil || err != nil {
 		t.Fatal("Failed: create method")
 	}
 	_, err = os.Open("../../database.json")
 	if err != nil {
-		t.Fatalf("Failed: %s", err.Error())
+		t.Fatalf("Test 'CreateDatabase' failed: %s", err.Error())
 	}
 }
 
+// gets a new chirp from the create chirp function
 func TestCreateChirp(t *testing.T) {
+	db, err := beforeEach()
+	chirp, err := db.CreateChirp("wow a chirp!")
+	if chirp == nil || err != nil {
+		t.Fatalf("Test 'CreateChirp' failed: %s", err.Error())
+	}
+}
+
+// read chirp
+// reads a chirp from the database
+func TestReadChirp(t *testing.T) {
 
 }
